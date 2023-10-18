e752adf3bb5d72bc4e786cf6e60874ef9ecc1b60
barath <82313227+Barath-2001@users.noreply.github.com>
2023-10-18 23:46:30 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -106,7 +106,7 @@ booked = 0;
 void Add() {
 booked += 1;
 }
-void SetBooked(const cars& c, Person& p) const {
+void SetBooked(const cars& c, const Person& p) const {
 ofstream Write;
 Write.open("Cars Info.txt", ios::app);
 if (Write.is_open()) {
@@ -701,7 +701,7 @@ cout << "\t\tEnter 4 for City 2017." << endl;
 cout << "\t\tEnter 5 to Exit." << endl;
 cout << endl;
 }
-void Show_Rent(const cars & c, const  Person & p, TotalRent & r, int check) {
+void Show_Rent(const cars & c, const  Person & p, const TotalRent & r, int check) {
 cout << "\n\t\t                       Car Rental - Customer Invoice                  " << endl;
 cout << "\t\t ///////////////////////////////////////////////////////////" << endl;
 cout << "\t\t | Invoice No. :" << "------------------|" << setw(18) << "#Cnb81353" << " |" << endl;
